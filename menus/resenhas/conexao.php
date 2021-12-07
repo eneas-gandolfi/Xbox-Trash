a9318c492599eaf482d9d21ee231e587ea9e437e
eneas-gandolfi <eneas.gandolfi@gmail.com>
2021-12-06 22:28:16 -0300

    atualizado

--- menus/resenhas/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Credenciais de acesso ao BD
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('DBNAME', 'resenha');
-
-$conn = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME . ';', USER, PASS);
